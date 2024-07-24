813c8e7bbfee07667f7b0d813b4b865bc0a32145
Matheus Gonçalves <matheus.pgon1@gmail.com>
2024-07-24 23:51:56 +0100

    Add new deploy script

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
     assetPrefix: isProd ? '/matheusPereiraG.github.io/' : '',
-    basePath: isProd ? '/matheusPereiraG.github.io' : '',
 };
 
 export default nextConfig;
