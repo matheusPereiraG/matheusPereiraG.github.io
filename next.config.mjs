11713fc7c334b7686011c133fd4672e4eb36f768
Matheus Gonçalves <matheus.pgon1@gmail.com>
2024-07-24 19:30:19 +0100

    Add asset prefix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export'
+    output: 'export',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/matheuspereirag.github.io' : '',
 };
 
 export default nextConfig;
