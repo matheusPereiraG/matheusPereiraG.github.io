bfcf317aa44dd8e630c9f5f6bc0f10bbe25a4337
matheusPereiraG <matheus.pgon1@gmail.com>
2024-09-23 10:36:21 +0100

    Add listener for tabs in home page

--- src/app/components/home-tabs.tsx
+++ src/app/components/home-tabs.tsx
@@ -1,45 +1,41 @@
-"use client";
-import { useState } from "react";
-
-interface HomeTabsProps {
-  className?: string; // optional className prop
+export type HomeTab = {
+  id: string,
+  name: string,
+  isSelected: boolean,
+  href: string,
 }
 
-export default function HomeTabs({ className }: HomeTabsProps) {
-    // Step 1: Create a state variable to track the selected tab
-    const [selectedTab, setSelectedTab] = useState<string>('About'); // Default selected tab
-  
-    // Step 2: Define the tabs (you can customize this if needed)
-    const tabs = [
-        { name: 'About', href: '#about' },          // Linking to section with id="about"
-        { name: 'Experience', href: '#experience' }, // Linking to section with id="experience"
-        { name: 'Projects', href: '#projects' },     // Linking to section with id="projects"
-      ];
-  
+export type HomeTabsProps = {
+  className?: string; // optional className prop
+  homeTabs: (HomeTab)[];
+  onClick: (id: string) => void;
+}
+
+export default function HomeTabs(props: HomeTabsProps) {
     return (
       <nav className="hidden lg:block">
-        <ul className={className}>
-          {tabs.map((tab) => (
+        <ul className={props.className}>
+          {props.homeTabs.map((tab) => (
             <li key={tab.name}>
               <a
                 href={tab.href}
-                onClick={() => setSelectedTab(tab.name)} // Step 3: Set the clicked tab as the selected one
+                onClick={() => props.onClick(tab.id)} // Step 3: Set the clicked tab as the selected one
                 className={`group flex items-center py-3 cursor-pointer ${
-                  selectedTab === tab.name
+                  tab.isSelected
                     ? 'text-slate-200' // Add active styles here
                     : 'text-slate-500'
                 }`}
               >
                 <span
                   className={`nav-indicator mr-4 h-px w-8 transition-all ${
-                    selectedTab === tab.name
+                    tab.isSelected
                       ? 'w-16 bg-slate-200' // Add active indicator styles here
                       : 'bg-slate-600'
                   }`}
                 ></span>
                 <span
                   className={`nav-text text-xs font-bold uppercase tracking-widest transition-all ${
-                    selectedTab === tab.name
+                    tab.isSelected
                       ? 'text-slate-200' // Active text color
                       : 'group-hover:text-slate-200'
                   }`}
@@ -53,39 +49,3 @@ export default function HomeTabs({ className }: HomeTabsProps) {
       </nav>
     );
   }
-  
-
-/*export default function HomeTabs({ className }: HomeTabsProps) {
-  return (
-    <>
-    <nav className="hidden lg:block">
-      <ul className={className}>
-        <li>
-          <a className="group flex items-center py-3">
-            <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-            <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-              About
-            </span>
-          </a>
-        </li>
-        <li>
-        <a className="group flex items-center py-3">
-            <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-            <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-              Experience
-            </span>
-          </a>
-        </li>
-        <li>
-        <a className="group flex items-center py-3">
-            <span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
-            <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">
-              Projects
-            </span>
-          </a>
-        </li>
-      </ul>
-      </nav>
-    </>
-  );
-}*/
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,81 @@
-import Image from "next/image";
+"use client";
+
 import Header from "./components/header";
 import PersonalHeader from "./components/personal-info-header";
-import HomeTabs from "./components/home-tabs";
+import HomeTabs, { HomeTab, HomeTabsProps } from "./components/home-tabs";
+import { useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
 
 export default function Home() {
+  const homeTabs: HomeTab[] = [
+    {
+      id: "1",
+      name: "About",
+      isSelected: true,
+      href: "#about",
+    },
+    {
+      id: "2",
+      name: "Experience",
+      isSelected: false,
+      href: "#experience",
+    },
+    {
+      id: "3",
+      name: "Projects",
+      isSelected: false,
+      href: "#projects",
+    },
+  ];
+
+  const [tabs, setTabs] = useState(homeTabs)
+
+  const updateActiveTab = (id: string) => {
+    setTabs(tabs.map(tab => ({
+      ...tab,
+      isSelected: tab.id === id
+    })));
+  };
+
+  // Set up `useInView` for each section
+  const { ref: aboutRef, inView: aboutInView } = useInView({
+    threshold: 0.5,
+  });
+
+  const { ref: experienceRef, inView: experienceInView } = useInView({
+    threshold: 0.5,
+  });
+
+  const { ref: projectsRef, inView: projectsInView } = useInView({
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (aboutInView) {
+      updateActiveTab("1");
+    } else if (experienceInView) {
+      updateActiveTab("2");
+    } else if (projectsInView) {
+      updateActiveTab("3");
+    }
+  }, [aboutInView, experienceInView, projectsInView]);
+
   return (
     <>
       <div className="flex">
-        <aside className="h-screen sticky top-0 p-20">
-        <div className="flex flex-col">
-          <PersonalHeader />
-          <HomeTabs className="mt-16 space-y-4" />
-        </div>
+        <aside className="h-screen sticky top-0 p-20 mr-10">
+          <div className="flex flex-col">
+            <PersonalHeader />
+            <HomeTabs
+              className="mt-16 space-y-4"
+              homeTabs={tabs}
+              onClick={(id: string) => {}}
+            />
+          </div>
         </aside>
 
-        <main className="p-20 ml-2">
-        <section id="about">
+        <main className="p-20">
+          <section id="about" ref={aboutRef}>
             <h1>About</h1>
             <p>
               he standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum
@@ -74,8 +134,8 @@ export default function Home() {
             </p>
           </section>
 
-          <section id="experience">
-          <h1>XP</h1>
+          <section id="experience" ref={experienceRef}>
+            <h1>XP</h1>
             <p>
               he standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum
               dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
@@ -133,8 +193,8 @@ export default function Home() {
             </p>
           </section>
 
-          <section id="projects">
-          <h1>Projects</h1>
+          <section id="projects" ref={projectsRef}>
+            <h1>Projects</h1>
             <p>
               he standard Lorem Ipsum passage, used since the 1500s "Lorem ipsum
               dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
@@ -193,109 +253,6 @@ export default function Home() {
           </section>
         </main>
       </div>
-    
-
-      {/* <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div> */}
     </>
   );
 }
