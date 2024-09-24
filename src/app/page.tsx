e3b54c8923455017cc8112d4106dfef3c6bce38e
matheusPereiraG <matheus.pgon1@gmail.com>
2024-09-24 16:49:04 +0100

    Changes to xl layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
           </div>
         </div>
 
-        <main className="flex flex-col mt-20 w-full px-4 lg:w-auto">
+        <main className="flex flex-col mt-20 w-full px-4 lg:w-auto xl:pr-20">
           <section id="about" ref={aboutRef} className="mb-20">
             <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
               <h2 className="text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only">
